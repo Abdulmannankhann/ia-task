2e22f068afab0624c91c7a041792369a85012651
Abdul9.khan <Abdul9.khan@ril.com>
2023-12-09 23:11:33 +0530

    Add chart title

--- src/components/cards/ChartCards.tsx
+++ src/components/cards/ChartCards.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import MuiLineChart from "components/liveCharts/MuiLineChart";
 import { SpectrumDatas } from "types/types";
+import { capitalizeFirstLetter } from "utils/functions";
 
 interface ChartCardsProps {
   colors: string[];
@@ -13,9 +14,11 @@ const ChartCards: React.FC<ChartCardsProps> = ({ colors, data, chartType }) => {
   return (
     <Card>
       <Card.Body>
-        <Card.Title className="">Altitude: {data.length > 0 ? data[data.length - 1]?.altitude.toFixed(2) + " km" : ""}</Card.Title>
+        <Card.Title className="">
+          {capitalizeFirstLetter(chartType)}: {data.length > 0 ? data[data.length - 1]?.altitude.toFixed(2) + " km" : ""}
+        </Card.Title>
         <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
-        <MuiLineChart colors={["#8884d8"]} data={data} chartType="altitude" />
+        <MuiLineChart colors={colors} data={data} chartType="altitude" />
         <div className="text-end m-2 mt-0 mb-0">
           <strong style={{ fontSize: "25px" }}>•</strong> Recent {data.length} values
         </div>
